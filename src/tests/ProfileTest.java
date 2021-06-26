d2485ac7b7410a580f44cf9b1b74c0fb7674e8ef
marijalukicni <marijalukicni@gmail.com>
2021-06-26 22:12:41 +0200

    Update ProfileTest

--- src/tests/ProfileTest.java
+++ src/tests/ProfileTest.java
@@ -20,11 +20,11 @@ public class ProfileTest extends BasicTest {
 		driver.get(baseUrl + "member/profile");
 		this.profilePage.changePersonalInformation("Robert", "Jones", "111 Maryland Ave.", "212-352-3212", "20850", "United States", "Maryland", "Rockville");
 		Thread.sleep(1000);
-		Assert.assertTrue(notificationSystemPage.getMessageText().contains("Setup Successful"), "[ERROR] Unexpected setup message");
+		Assert.assertTrue(this.notificationSystemPage.getMessageText().contains("Setup Successful"), "[ERROR] Unexpected setup message");
 
 		this.authPage.logout();
 		Thread.sleep(500);
-		Assert.assertTrue(notificationSystemPage.getMessageText().contains("Logout Successfull"), "[ERROR] Unexpected logout message");
+		Assert.assertTrue(this.notificationSystemPage.getMessageText().contains("Logout Successfull"), "[ERROR] Unexpected logout message");
 		
 	}
 	
@@ -39,18 +39,18 @@ public class ProfileTest extends BasicTest {
 		Assert.assertTrue(this.notificationSystemPage.getMessageText().contains("Login Successfull"), "[ERROR] Unexpected login message");
 		
 		driver.get(baseUrl + "member/profile");
-		String imgPath = new File("img/ITBootcamp.png").getCanonicalPath();
-		this.profilePage.uploadImg(imgPath);
+		String imagePath = new File("img/ITBootcamp.png").getCanonicalPath();
+		this.profilePage.uploadImg(imagePath);
 		Thread.sleep(500);
-		Assert.assertTrue(notificationSystemPage.getMessageText().contains("Profile Image Uploaded Successfully"), "[ERROR] Upload image failed");
+		Assert.assertTrue(this.notificationSystemPage.getMessageText().contains("Profile Image Uploaded Successfully"), "[ERROR] Upload image failed");
 		this.notificationSystemPage.waitUntilMessageDisappears();
 		
 		this.profilePage.removeImage();
-		Assert.assertTrue(notificationSystemPage.getMessageText().contains("Profile Image Deleted Successfully"), "[ERROR] Delete image failed");
+		Assert.assertTrue(this.notificationSystemPage.getMessageText().contains("Profile Image Deleted Successfully"), "[ERROR] Delete image failed");
 		this.notificationSystemPage.waitUntilMessageDisappears();
 		
 		this.authPage.logout();
-		Assert.assertTrue(notificationSystemPage.getMessageText().contains("Logout Successfull"), "[ERROR] Unexpected logout message");
+		Assert.assertTrue(this.notificationSystemPage.getMessageText().contains("Logout Successfull"), "[ERROR] Unexpected logout message");
 		this.notificationSystemPage.waitUntilMessageDisappears();
 		
 	}
